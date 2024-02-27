408186434a7c8b128dd00c727ec64d12f8f4d344
Luiz Gabriel Ferreira <luiz.gabriel.lcf@gmail.com>
2024-02-27 12:34:51 -0300

    tabuada

--- index.php
+++ index.php
@@ -1,52 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Exemplo PHP</title>
+</head>
+<body>
 
-//exemplo de criação de variáveis
-$n1 = 10;
-$n2 = 2;
-$texto = "Mensagem de Texto";
-
-//exemplo de exibição de variáveis
-echo $texto . "<br>";
-echo $n1 + $n2 . "<br>";
-
-//exemplos de operações com variáveis
-echo "$n1 + $n2 = " . $n1 + $n2 . "<br>";
-echo "$n1 - $n2 = " . $n1 - $n2 . "<br>";
-echo "$n1 * $n2 = " . $n1 * $n2 . "<br>";
-echo "$n1 / $n2 = " . $n1 / $n2 . "<br>";
-
-//exemplo de decisão
-if ($n1 <= 10) {
-    echo "O número é menor ou igual a 10 <br>";
-} else {
-    echo "O número é maior que 10 <br>";
-}
-
-//estrutura de repetição
-echo "<br> Estrutura de Repetição <br>";
-for($n1 = 0; $n1 <= 10; $n1++){
-    echo $n1 . "<br>";
-}
-
-echo "<br>";
-
-/*
-TABUADA DO 2 - EXEMPLO
-
-echo "Tabuada do 2 <br>";
-for($i = 0; $i <= 10; $i++){
-    echo 2 * $i . "<br>";
-}
-echo "<br>";
-*/
-
-//tabuada completa
-for($i = 1; $i <= 10; $i++){
-    echo "Tabuada do " . $i . "<br>";
-    for($j = 0; $j <= 10; $j++){
-        echo "$i * $j = " . $i * $j . "<br>";
-    }
-    echo "<br>";
-}
-
-?>
+    <table class="table">
+        <tbody>
+            <?php for($i = 0; $i <= 100; $i++){ ?>
+            <tr>
+                <?php for($j = 0; $j <= 10; $j++){ ?>
+                        <td>
+                        <?php echo $i . " x " . $j . " = " . $i*$j; ?>
+                        </td>
+                <?php } ?>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    
+</body>
+</html>
